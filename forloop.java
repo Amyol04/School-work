ba379b53aabc8ab60edb3a9aa5d8e6488ba40286
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-03 14:48:06 +0100

    Update forloop.java

--- forloop.java
+++ forloop.java
@@ -1,14 +1,13 @@
 
 public class forloop {
 	public static void main(String[] args) {
-		
-		for (int i = 1; i < 10; i++) {
 			
-		if(i % 2 == 0)	 
-			
-			System.out.println(i);
-			
-		} // end of for/ if statement
-		
-	} // end of main
+		for (int i = 1; i < 10; i++) {	
+			if(i % 2 == 0)	{
+				
+					System.out.println(i);
+			}//end if
+				
+	  } // end of for 			
+   } // end of main
 } // end of class
