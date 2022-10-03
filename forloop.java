5f8f2caa6dd6212507d9c1618151808fbbecaae4
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-03 10:53:45 +0100

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop.java {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println(i);
+		} // end of for 
+	} // end of main
+} // end of class
