5d7dfdd128ac9936032f2cad3401a67de7429c88
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-03 14:39:34 +0100

    Add files via upload

--- forloop.java
+++ forloop.java
@@ -1,9 +1,14 @@
-public class forloop.java {
+
+public class forloop {
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 5; i++) {
+		for (int i = 1; i < 10; i++) {
+			
+		if(i % 2 == 0)	 
 			
 			System.out.println(i);
+			
 		} // end of for 
+		
 	} // end of main
 } // end of class
