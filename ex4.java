add85f977681def94d35bb4d618b9ef87aed3b98
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-03 09:27:03 +0100

    Create ex4.java

---
+++ ex4.java
@@ -0,0 +1,38 @@
+public class ex4 {
+	public static void main(String[] args) {
+		
+		int Var1 =20;
+		int Var2 =43;
+		int Var3 =(Var1/Var2);
+		int Var4 =(Var1*Var1);
+		
+		String StrA="first";
+        String StrB="seccond";
+        String StrC=(StrA+StrB);
+        
+        
+        if(Var3 < Var4) {
+        	
+        	System.out.println("var3 is less than or equal to var4");
+        	
+        	
+        }
+        
+        else {
+        	
+        	System.out.println("var3 is more then var4");
+        	
+        }
+        
+        if(StrA == StrC ) {
+        	
+        	System.out.println("strings are equal ​");
+        	
+        }
+        
+        
+    
+        
+        
+	} // end of class
+} //end of main
