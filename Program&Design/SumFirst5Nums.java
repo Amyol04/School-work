f5abe8f4b1918a2d225dce2c9780403dce9f9dc1
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-25 15:29:43 +0100

    Add files via upload

---
+++ Program&Design/SumFirst5Nums.java
@@ -0,0 +1,18 @@
+public class SumFirst5Nums {
+        public static void main(String []args) {
+            
+            
+            int sum = 0;
+            int i;
+            for ( i = 0; i <= 5; i++){	
+            
+                System.out.println("first 5 real nums are:" +sum);
+                sum = sum+i;
+                
+                } // end of for statement 
+            
+            System.out.println("The sum of the first 5 real numbers are: "+sum);
+            
+        } // end of main
+ } // end of class
+
