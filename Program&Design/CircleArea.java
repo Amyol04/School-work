c6c07499026187bb3db6f5d17cfd1755e21805d0
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-10-26 12:37:48 +0100

    Add files via upload

---
+++ Program&Design/CircleArea.java
@@ -0,0 +1,11 @@
+public class CircleArea {
+
+    public static void main(String []args){
+        double radius = 3.67;
+        double pi  = 3.14;
+     
+        double area = pi + Math.pow(radius, 2);
+       System.out.println("Area of Circle is:" +area);
+
+    }// main
+}// class
