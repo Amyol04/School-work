8ed58c504f06cf244cedb68910be4a69d3041484
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-11-21 15:37:51 +0000

    Update hightArray.java

--- Program&Design/javaArrayPorjectEx/hightArray.java
+++ Program&Design/javaArrayPorjectEx/hightArray.java
@@ -1,3 +1,7 @@
+package javaArrayPorjectEx;
+
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 /**
@@ -10,9 +14,19 @@ import javax.swing.JOptionPane;
  */
 
 public class hightArray {
+	
 	public static void main(String[] args) {
+		
 		double[] studentHeights = null;
+		double smallestHeight = 0.0;
+		String userMessage = "";
+		
 		studentHeights = fillArray(studentHeights);
+		smallestHeight = findSmallestHeight(studentHeights);
+		//largest height
+		//average height
+		userMessage = createUserMessage(studentHeights, smallestHeight);
+		JOptionPane.showMessageDialog(null, userMessage);
 	} // end of main
 	
 	public static double[] fillArray(double[] heightArray) {
@@ -36,5 +50,26 @@ public class hightArray {
 		
 		
 	} // end of method
+	
+	public static double findSmallestHeight(double[] heightArray) {
+        
+        Arrays.sort(heightArray);
+        
+        return heightArray[0];
+        
+    } // end of smallest 
+
+	public static String createUserMessage(double[] heightArray, double smallestHeight) {
+		
+		String userMessage ="";
+		
+		userMessage = userMessage.concat("height details for your class are as follow: \n");
+		for(int i = 0; i < heightArray.length; i++) {
+            userMessage = userMessage.concat("height "+i+"is: "+heightArray[i]+"\n");
+        }
+        userMessage = userMessage.concat("Smallest height is: " +smallestHeight + "\n");
+    
+        return userMessage;
+	} // end of user message
 	
 } // end of class
