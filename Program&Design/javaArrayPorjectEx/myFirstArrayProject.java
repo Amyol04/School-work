2c9f5f8d0125fe57f8c074e8f5b171551136437f
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-11-21 09:41:45 +0000

    Update myFirstArrayProject.java

--- Program&Design/javaArrayPorjectEx/myFirstArrayProject.java
+++ Program&Design/javaArrayPorjectEx/myFirstArrayProject.java
@@ -3,8 +3,15 @@ public class myFirstArrayPorject {
 	 
 	 //CREATE AN ARRAY W/O INITILISING
 	 //FILLED ARRAY POSITION [0],[1],[2],[3]
-	 String[] cars = {"volvo","BMW" ,"Porche","VW"};
+	 String[] cars = {"volvo","Skoda" ,"Porche","VW"};
 	 
+	 // read value at index position
 	 System.out.println("Arrray potiton 1 contains:" +cars[0]);
+	 
+	 // updating a value at an index position 
+	 cars[1] = "mercedes";
+	 
+	 System.out.println("Array posotion 2 has changed to: "+ cars[1]);
+	
  } // end of main
 } // end of class
