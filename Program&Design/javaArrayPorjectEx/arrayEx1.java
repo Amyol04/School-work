d639832741492590c07e9fa4d297ed55c835ba4b
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-11-21 10:45:54 +0000

    Create arrayEx1.java

---
+++ Program&Design/javaArrayPorjectEx/arrayEx1.java
@@ -0,0 +1,15 @@
+public class arrayEx1 {
+   public static void main(String[] args) {
+	   
+	   String[] cars = {"Toyota","BMW" ,"Porche","VW", "Audi"};
+	   
+	   
+	   cars[2] = "Honda Blue";
+	   
+	   for (int i = 0; i < cars.length; i++) {
+           System.out.println(cars[i]);
+       } //end of for
+	   
+	   
+   } // end of main
+} // end of class
