22379bca893996e515bab6fb87354116dfdf40b1
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-11-21 09:37:51 +0000

    Create myFirstArrayProject.java

---
+++ Program&Design/javaArrayPorjectEx/myFirstArrayProject.java
@@ -0,0 +1,10 @@
+public class myFirstArrayPorject {
+ public static void main(String[] args) {
+	 
+	 //CREATE AN ARRAY W/O INITILISING
+	 //FILLED ARRAY POSITION [0],[1],[2],[3]
+	 String[] cars = {"volvo","BMW" ,"Porche","VW"};
+	 
+	 System.out.println("Arrray potiton 1 contains:" +cars[0]);
+ } // end of main
+} // end of class
