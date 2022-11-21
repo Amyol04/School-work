d36e7fd8a2d3f1f8f615ab50deec445f605989e9
Amy! <114475454+Amyol04@users.noreply.github.com>
2022-11-21 09:54:50 +0000

    Update myFirstArrayProject.java

--- Program&Design/javaArrayPorjectEx/myFirstArrayProject.java
+++ Program&Design/javaArrayPorjectEx/myFirstArrayProject.java
@@ -4,6 +4,7 @@ public class myFirstArrayPorject {
 	 //CREATE AN ARRAY W/O INITILISING
 	 //FILLED ARRAY POSITION [0],[1],[2],[3]
 	 String[] cars = {"volvo","Skoda" ,"Porche","VW"};
+	 int[] ages = {12 , 5 , 35, 25, 16};
 	 
 	 // read value at index position
 	 System.out.println("Arrray potiton 1 contains:" +cars[0]);
@@ -12,6 +13,18 @@ public class myFirstArrayPorject {
 	 cars[1] = "mercedes";
 	 
 	 System.out.println("Array posotion 2 has changed to: "+ cars[1]);
+	 
+	 // length of array
+	 System.out.println("Size of array " +cars.length);
+	 
+	 Arrays.sort(ages);
+	 
+	 for(int i = 0; 1 < ages.length; i++) {
+		 
+		 System.out.println("Cars element "+ i+ "is"+ ages[i]);
+		 
+		 
+	 } // end for 
 	
  } // end of main
 } // end of class
