0fef68db2d1184aa7e9711816a1530f1c3c9c83e
Cysticmoss <72623940+Cysticmoss@users.noreply.github.com>
2022-11-24 15:30:47 +0000

    Create CmToInch.java

---
+++ softwareArch/CmToInch.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class cmtoinch {
+
+public static void main(String[] args) {
+		
+		double cm, inch;
+		String input, output;
+		
+		//Get the temp in  
+		input = JOptionPane.showInputDialog("Enter a temp in \u00b0Cm");
+		cm = Double.parseDouble(input);
+		
+		//Convert from C to F
+		inch = cm/ 2.54;
+		
+		//Output the Temp in F
+		output = String.format("%3f\u00b0Cm is  %3f\u00b0Inch", cm, inch);
+		JOptionPane.showMessageDialog(null, output);
+	
+}
+
+
+}
