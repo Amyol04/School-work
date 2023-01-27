9561ce5db5f4c761ff49ab4189109e596509469d
Amy! <114475454+Amyol04@users.noreply.github.com>
2023-01-27 12:36:00 +0000

    Update testTime.java

--- fundamentalsOfFoop/Time/testTime.java
+++ fundamentalsOfFoop/Time/testTime.java
@@ -1,14 +1,19 @@
+
 public class TestTime {
     
     public static void main(String args[]){
+
+        int hours, minutes, seconds;
+        Scanner keyboard = new Scanner(System.in);
+        
         	
         Timee firstClock = new Timee();
         System.out.println(firstClock);
 
-        for(int i = 0; i < 24; i++) {
-			firstClock.inc();
+        for(int i = 0; i < 60; i++) {
+			firstClock.tick();
 			System.out.println(firstClock);
 		}
-    }
+    } // end of mian
     
-}
+} // end of class 
