68af52e7f93b84162cb2b466855449c04b545e24
Amy! <114475454+Amyol04@users.noreply.github.com>
2023-01-30 15:35:15 +0000

    Update testTime.java

--- fundamentalsOfFoop/Time/testTime.java
+++ fundamentalsOfFoop/Time/testTime.java
@@ -1,16 +1,11 @@
-
 public class TestTime {
     
     public static void main(String args[]){
 
-        int hours, minutes, seconds;
-        Scanner keyboard = new Scanner(System.in);
-        
-        	
         Timee firstClock = new Timee();
         System.out.println(firstClock);
 
-        for(int i = 0; i < 60; i++) {
+        for(int i = 0; i < 8000; i++) {
 			firstClock.tick();
 			System.out.println(firstClock);
 		}
