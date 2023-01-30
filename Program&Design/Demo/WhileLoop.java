c0ba154760927243e6811752801e2d67cd4ec9bf
Amy! <114475454+Amyol04@users.noreply.github.com>
2023-01-30 15:01:22 +0000

    Create WhileLoop.java

---
+++ Program&Design/Demo/WhileLoop.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+
+	int i = 0;
+	
+	while(i <= 5) {
+		
+		System.out.println("Value of counter is: " +i);
+		
+		i++;
+	
+     	} // end of while 
+	
+	} // end of main 
+	
+} // end of class
