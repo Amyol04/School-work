6b620e6f64120199d3206870990976619a9ddd62
Amy! <114475454+Amyol04@users.noreply.github.com>
2023-01-30 15:16:45 +0000

    Create multiplierEx2.java

---
+++ Program&Design/exercise/multiplierEx2.java
@@ -0,0 +1,10 @@
+public class multiplierEx2 {
+	public static void main(String[] args) {
+		double myNum =20.2;
+		double mulitpier = 2.4;
+		
+		double sum = myNum + mulitpier;
+		
+		System.out.println("sum is"+sum);
+	  } // end of main
+} // end if class 
