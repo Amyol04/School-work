97d50115aca1ce63f98819486d3debabc5db8825
Amy O Leary! <amyoleary404@gmail.com>
2023-03-21 15:50:18 +0000

    Update Driver.java

--- fundamentalsOfFoop/CarGarage/Driver.java
+++ fundamentalsOfFoop/CarGarage/Driver.java
@@ -11,46 +11,46 @@ package carGarage;
  *  The efficiency method takes in 2 vehicle objects as a parameter and print which is most efficient to screen using a dialog box.
  *
  * */
+import javax.swing.JOptionPane;
 
 public class Driver {
-	
-	public static void main(String[] args) {
-		
-		Bus myBus = new Bus("Bus100", "132 D 24", 0, 400, 2.5, 23);
-		Truck myTruck = new Truck("Bus100", "132 D 24", 0, 400, 2.5, 4324);
-		
-		System.out.println("Fuel needed to drive the bus 150km" +myBus.fuelNeeded(100)); 
-		myBus.display();
-		System.out.println("Fuel needed to drive the truck 150km" +myTruck.fuelNeeded(100));
-		myTruck.display();
-		
-		System.out.println("The bus travels " +myBus.distanceCoveredKM(1)+ " in one hour");
-		System.out.println("The truck travels " +myTruck.distanceCoveredKM(1)+ " in one hour");
-		
-		efficiencycalculator(myBus, myTruck);
-		
-	}
+    public static void main(String[] args) {
+        // create an instance of class Truck
+        Truck myTruck = new Truck("Ford F-150", "ABC123", 120, 50, 10, 2000);
+        // create an instance of class Bus
+        Bus myBus = new Bus("Toyota Coaster", "XYZ456", 80, 80, 8,45);
+        
+        // call fuelNeeded() and display() methods using objects of these classes
+        double fuelNeededForTruck = myTruck.fuelNeeded(100);
+        double fuelNeededForBus = myBus.fuelNeeded(100);
+        JOptionPane.showMessageDialog(null, "Fuel needed for Truck to travel 100 km: " + fuelNeededForTruck + " litres\n"
+                                            + "Fuel needed for Bus to travel 100 km: " + fuelNeededForBus + " litres");
+        myTruck.display();
+        myBus.display();
 
-	public static Vehicle efficiencycalculator (Vehicle v1, Vehicle v2) {
+		 
+		System.out.println("The bus travels " + myBus.distanceCovered(1) + " in 1 hour");	 //distanceCoveredKM for Bus
+		System.out.println("The truck travels " + myTruck.distanceCovered(1) + " in 1 hour");	 //distanceCoveredKM for Truck
 
+        
+        // use a method called efficiencyCalculator() to calculate which of 2 vehicle objects is the more efficient
+        Vehicle moreEfficientVehicle = efficiencyCalculator(myTruck, myBus);
+        JOptionPane.showMessageDialog(null, "The " + moreEfficientVehicle.getModel() + " is the more efficient vehicle.");
+    }
+    
+    public static Vehicle efficiencyCalculator(Vehicle v1, Vehicle v2) {
         double fuelPerKm1 = 1 / v1.getFuelConsumption();
         double fuelPerKm2 = 1 / v2.getFuelConsumption();
-        double distance1 = v1.distanceCoveredKM(1);
-        double distance2 = v2.distanceCoveredKM(1);
+        double distance1 = v1.distanceCovered(1);
+        double distance2 = v2.distanceCovered(1);
         double efficiency1 = distance1 / fuelPerKm1;
         double efficiency2 = distance2 / fuelPerKm2;
-    
-
-		if (efficiency1 > efficiency2) {
-
+        
+        if (efficiency1 > efficiency2) {
             return v1;
-            
         } else {
-
-    return v2; 
-
-  	  }
-	}		
+            return v2;
+        }
+    }
 }
-
 
